3f9c0fa4a5a1f3982b697f2f16d44006151b7f21
Somebody Somebodious <cents02@github.com>
2020-05-24 07:49:58 -0700

    Fixed ooc being logged despite setting

--- server/client_manager.py
+++ server/client_manager.py
@@ -586,9 +586,10 @@ class ClientManager:
         for c in self.server.client_manager.clients:
             if c.ipid == temp_ipid:
                 c.clientscon -= 1
-        self.clients.remove(client)
+        
         if client in client.area.afkers:
             client.area.afkers.remove(client)
+        self.clients.remove(client)
     def get_targets(self, client, key, value, local=False, single=False):
         """
         Find players by a combination of identifying data.
