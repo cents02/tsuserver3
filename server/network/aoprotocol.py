e53a0fc705914f0169c19b8be49940050f8b8738
cents <sens03.con@gmail.com>
2020-05-27 13:56:33 -0700

    Modcall dumps buffer

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -24,6 +24,7 @@ import logging
 logger_debug = logging.getLogger('debug')
 logger = logging.getLogger('events')
 
+
 from enum import Enum
 
 import arrow
@@ -604,7 +605,9 @@ class AOProtocol(asyncio.Protocol):
         self.client.area.set_next_msg_delay(len(msg))
         if(bool(self.server.config['log_chat'])):
             database.log_ic(self.client, self.client.area, showname, msg)
-
+        else:
+            database.log_buffer(f'[{self.client.area.abbreviation}] {showname}/{self.client.char_name}' +
+                          f'/{self.client.name} ({self.client.ipid}): {msg}')
         if (self.client.area.is_recording):
             self.client.area.recorded_messages.append(args)
 
@@ -689,7 +692,9 @@ class AOProtocol(asyncio.Protocol):
                 args[1])
             if(bool(self.server.config['log_chat'])):
                 database.log_room('ooc', self.client, self.client.area, message=args[1])
-
+            else:
+                database.log_buffer('[OOC]' + f'[{self.client.area.abbreviation}] {self.client.char_name}' +
+                          f'/{self.client.name} ({self.client.ipid}): {args[1]}')
     def net_cmd_mc(self, args):
         """Play music.
 
@@ -987,7 +992,7 @@ class AOProtocol(asyncio.Protocol):
                 pred=lambda c: c.is_mod)
             self.client.set_mod_call_delay()
             database.log_room('modcall', self.client, self.client.area, message=args[0])
-
+        database.dump_log(self.client.area.abbreviation,args[0],self.client)
     def net_cmd_opKICK(self, args):
         """
         Unused; kick a user from the client UI.
